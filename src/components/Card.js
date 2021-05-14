1b27be37923bb973325aafda3f23d92ec095db1d
Brandon Coates <bran235@hotmail.com>
2021-05-14 16:16:01 +0100

    Fixed the bug with the buy not working as intended

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,19 @@ import './Card.css'
 
 
 const Card = (props) => {
+
+  const handleButton = (e) => {
+    e.stopPropagation(); 
+    e.preventDefault();
+  }
+
   return (
     <div className="card" id={props.id} onClick={props.onClickDiv}>
       <img id={props.imgId} className="cat-image" src={props.imgSrc} alt="cat" />
       <div className="catDetails" id={props.catDetails}>
         <h2>{props.name}</h2>
         <p>{props.price}</p>
-        <div className="btnContainer" id={props.buttonId} onClick={(e) => e.stopPropagation()}>
+        <div className="btnContainer" id={props.buttonId} onClick={handleButton}>
           <button onClick={props.cart}>Buy</button>
         </div>
       </div>
