9f1c4d79e6cc6a5805fe0380850c4f81990843d6
Brandon Coates <bran235@hotmail.com>
2021-05-14 15:28:43 +0100

    Added a little react router, no switch because it's not needed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import "./Sidebar.css"
 
-const Sidebar = ({ width, height, children, hidden, setHidden}, props) =>{
+const Sidebar = ({ width, height, children, hidden, setHidden}, props) => {
     const [xPosition, setX] = React.useState(-width);
 
 //checks if the xPosition is negative = hidden sidepanel 
@@ -14,19 +14,21 @@ const Sidebar = ({ width, height, children, hidden, setHidden}, props) =>{
 	}
 //useeffect will update the position of the sidepanel 
     React.useEffect(()=>{
-        setX(-width);
-				if (!hidden) {
-					togglePanel();
-				}
+			setX(-width);
+			if (!hidden) {
+				togglePanel();
+			}
 		}, []);
 
 		const handleButton = () => {
 			togglePanel(); 
-			setHidden(!hidden);
+			setTimeout(() => {
+				setHidden(!hidden);
+			}, 1000);
 		}
 
     return(
-    <div className="sidepanel" style={{ zIndex: `${xPosition < 0 ? "-1" : "1"}` }}>
+    <div className="sidepanel" style={{ /* zIndex: `${xPosition < 0 ? "-1" : "1"}` */ }}>
         <div className="side-bar" style={{ width: width, minHeight: height, transform: `translatex(${xPosition}px)` }}>
             <button onClick={handleButton} id="test" className="side-bar-btn" style={{transform: `translate(${width}px, 20vh)` }}></button>
             <div className="side-bar-content">{children}</div>
